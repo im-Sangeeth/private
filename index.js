66b4e3bef172dd65f40bbb7a4d1671dbfc6764eb
im-Sangeeth <115408435+im-Sangeeth@users.noreply.github.com>
2023-07-06 23:12:56 +0530

    v0.01

---
+++ index.js
@@ -0,0 +1,11 @@
+function vali(display){
+    if(display.value=='aishu'){
+        display.value='🩷';
+    }
+    if(display.value=='rupa'){
+        display.value='💚';
+    } if(display.value=='sumi'){
+        display.value='❤️';
+    }
+    display.value=eval(display.value);
+}
